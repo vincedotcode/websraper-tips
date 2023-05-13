88a198a0a8019b7191202e7d6f94e929ac48e1aa
Vince Erkadoo <erkadoovince@gmail.com>
2023-05-13 16:49:29 +0400

    ss

--- email.js
+++ email.js
@@ -3,18 +3,19 @@ var defaultClient = SibApiV3Sdk.ApiClient.instance;
 
 // Configure API key authorization: api-key
 var apiKey = defaultClient.authentications['api-key'];
-apiKey.apiKey = process.env.BREVO_API_KEY;
+apiKey.apiKey = process.env.BREVO_API_KEY; 
 
-var apiInstance = new SibApiV3Sdk.SMTPApi();
+var apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
 async function sendEmail(data) {
   const { name, email, mobile, message } = data;
 
-  let sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
-  sendSmtpEmail.to = [{ email: 'betgramorg@gmail.com' }];
-  sendSmtpEmail.sender = { email: 'erkadoovince@gmail.com' };
-  sendSmtpEmail.subject = 'New Contact Form Submission';
-  sendSmtpEmail.textContent = `Name: ${name}\nEmail: ${email}\nMobile: ${mobile}\nMessage: ${message}`;
+  let sendSmtpEmail = {
+    to: [{ email: 'betgramorg@gmail.com' }],
+    sender: { email: 'erkadoovince@gmail.com' },
+    subject: 'New Contact Form Submission',
+    textContent: `Name: ${name}\nEmail: ${email}\nMobile: ${mobile}\nMessage: ${message}`,
+  };
 
   try {
     const result = await apiInstance.sendTransacEmail(sendSmtpEmail);
